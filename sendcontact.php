2d4f3b98e95686bbe4340b5b2ce0589226b14e35
git <smendoza.barrera@gmail.com>
2016-03-16 18:16:11 -0600

    Email test v0.0.6

--- sendcontact.php
+++ sendcontact.php
@@ -36,6 +36,6 @@ if ( $success )
 }
 else
 {
-    echo "<h1>There was a problem sending your message. Please try again.</h1>";
+    echo "<h2>The Id Store field accept numbers only. Please try again. <a href="http://www.baxibao.cn">Back...</a> </h2>";
 }
 ?>
