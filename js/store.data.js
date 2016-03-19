4579ee7e6c7d35edcc45dace48a4067a06994a57
git <smendoza.barrera@gmail.com>
2016-03-19 16:49:42 -0600

    Event handler creation v0.0.16

--- js/store.data.js
+++ js/store.data.js
@@ -3,7 +3,7 @@
 document.getElementById('contactForm').addEventListener('submit', readStoreData);
 
 function readStoreData(event) {
-    event.preventDefault();
+    // event.preventDefault();
     console.log('Your variables of interest:');
     console.log(document.getElementById('idstore').value);
     console.log(document.getElementById('email').value);
