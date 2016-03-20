00e2ab02eb874150cf47fbab22f92c61cb534daf
git <smendoza.barrera@gmail.com>
2016-03-19 20:37:02 -0600

    Test variable pass to thanks page v0.0.23

--- js/results.js
+++ js/results.js
@@ -1,3 +1,6 @@
 function myResults() {
-    alert("Page is loaded");
+    var storeNumber = sessionStorage.passUrl;
+
+    document.getElementById('results').innerHTML = 'Dear supplier, your store: ' + storeNumber + ' will be processed to submission,<br>we will send an email in 24 hours maximum!<br>Thank you!';
+    // alert("Page is loaded");
 }
